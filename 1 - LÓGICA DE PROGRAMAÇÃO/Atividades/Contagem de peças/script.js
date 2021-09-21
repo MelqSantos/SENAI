0a1f6addc451e11c63bdca015ac97589d5642c88
Melqui Santos <melqsantos96@gmail.com>
2021-09-21 17:54:40 -0300

    :heavy_check_mark: Ex: Contagem de peças

--- 1 - LÓGICA DE PROGRAMAÇÃO/Atividades/Contagem de peças/script.js
+++ 1 - LÓGICA DE PROGRAMAÇÃO/Atividades/Contagem de peças/script.js
@@ -1,5 +1,8 @@
+/* ------------------------- Sistema de cadastro de peças ------------------------- */
+
 var listaDePecas = [];
-// Pergunta inicial
+
+// Pergunta inicial, para definir a quantidade de itens na lista de peças.
 var quantidadeLista = window.prompt(`+ ---------- Parâmetros para cadastro ---------- +
 - Limite de 10 itens na lista
 - Peças com peso superior a 100g
@@ -8,54 +11,70 @@ var quantidadeLista = window.prompt(`+ ---------- Parâmetros para cadastro ----
 Quantas peças deseja cadastrar?
 `);
 
+// Validação inicial para não ultrapassar o limite de 10 peças.
 if (quantidadeLista > 10) {
-    alert("[ERRO] Só é possível cadastrar no máximo 10 itens!")
+    alert("[ERRO] Só é possível cadastrar no máximo 10 itens!");
 } else {
+
+    // Laço de repetição para as perguntas.
     for (var x = 0; x < quantidadeLista; x++) {
 
         var peca = window.prompt(`Digite o nome da peça n°${x + 1} :`);
-
+        // Validação de quantidade de caracteres.
         if (peca.length < 3) {
             window.confirm(`* ------- Peça deverá possuir mais de 3 caracteres! ------- *
                     [ATENÇÃO] 1 tentativa restante!`)
 
             peca = window.prompt("Digite novamente o nome da peça:");
-
         }
-
+        // 2° Validação caso o usuário digite novamente a mesma coisa.
         if (peca.length < 3) {
-            alert("[ATENÇÃO] Cadastro encerrado por esgotamento de tentativas!")
+            alert("[ATENÇÃO] Cadastro encerrado por esgotamento de tentativas!");
+            break;
+
         } else
 
             var peso = Number(window.prompt(`Digite o peso da peça "${peca}" em gramas:`));
-
+        // Validação de peso da peça
         if (peso < 100) {
             window.confirm(`* ------- Peça deverá possuir peso superior a 100g! ------- *
-                    [ATENÇÃO] 1 tentativa restante!`)
+                    [ATENÇÃO] 1 tentativa restante!`);
 
             peso = Number(window.prompt(`Digite novamente o peso da peça "${peca}" em gramas:`));
-
         }
+        // 2° Validação caso o usuário digite novamente a mesma coisa.
         if (peso < 100) {
-            alert("[ATENÇÃO] Cadastro encerrado por esgotamento de tentativas!")
+            alert("[ATENÇÃO] Cadastro encerrado por esgotamento de tentativas!");
+            break;
+
         } else {
 
+            // Cria um objeto dentro da lista com as características da peça atual. 
             listaDePecas.push({
                 desc: peca,
                 peso: peso
             });
-
         }
     }
-
-    if (listaDePecas.length != 0)
-        alert("[Parabéns] Peças cadastradas com sucesso!")
 }
 
-// Mostar peças
+/* ------- Cria uma tabela para mostrar os itens cadastrados ------- */
 
-for (var i = 0; i < quantidadeLista; i++) {
-    document.write(`
-    Peça: ${listaDePecas[i].desc} | Peso: ${listaDePecas[i].peso}g <br>`)
+var tabela = document.getElementById("table");
 
+for (var x = 0; x < quantidadeLista; x++) {
+    // Cria o elemento "tr" (linha), adiciona um ID e incrementa esse elemento na tabela.
+    var linha = document.createElement("tr");
+    linha.setAttribute("id", "tr" + x)
+    tabela.appendChild(linha);
+
+    // Cria elemento "td", adiciona o nome da peça no elemento e adiciona na 1° célula. 
+    var conteudo = document.createElement("td");
+    conteudo.innerHTML = listaDePecas[x].desc;
+    document.getElementById("tr" + x).appendChild(conteudo);
+
+    // Cria elemento "td", adiciona o peso da peça no elemento e adiciona na 2° célula.
+    conteudo = document.createElement("td");
+    conteudo.innerHTML = listaDePecas[x].peso + "g";
+    document.getElementById("tr" + x).appendChild(conteudo);
 }
