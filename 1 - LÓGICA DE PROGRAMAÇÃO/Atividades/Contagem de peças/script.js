7b3ebdbbc039d8e8a0cf4f449294164afbef682d
Melqui Santos <melqsantos96@gmail.com>
2021-09-21 16:19:36 -0300

    :pencil2: Atualização de exercício

--- 1 - LÓGICA DE PROGRAMAÇÃO/Atividades/Contagem de peças/script.js
+++ 1 - LÓGICA DE PROGRAMAÇÃO/Atividades/Contagem de peças/script.js
@@ -1,28 +1,60 @@
-let listaDePecas = [];
-let tentativas = window.prompt("Quantas peças deseja cadastrar?");
-let peca_tabela = document.getElementById("desc");
-let peso_tabela = document.getElementById("peso");
+var listaDePecas = [];
+// Pergunta inicial
+var quantidadeLista = window.prompt(`+ ---------- Parâmetros para cadastro ---------- +
+- Limite de 10 itens na lista
+- Peças com peso superior a 100g
+- Peças com mais de 3 caracteres no nome
 
-if (tentativas > 10) {
+Quantas peças deseja cadastrar?
+`);
+
+if (quantidadeLista > 10) {
     alert("[ERRO] Só é possível cadastrar no máximo 10 itens!")
 } else {
+    for (var x = 0; x < quantidadeLista; x++) {
 
-    for (let x = 0; x < tentativas; x++) {
+        var peca = window.prompt(`Digite o nome da peça n°${x + 1} :`);
 
-        let peca = window.prompt("Digite uma peça:");
-        let peso = window.prompt("Digite o peso da peça anterior:");
+        if (peca.length < 3) {
+            window.confirm(`* ------- Peça deverá possuir mais de 3 caracteres! ------- *
+                    [ATENÇÃO] 1 tentativa restante!`)
 
-        listaDePecas.push({
-            desc: peca,
-            peso: peso
-        });
+            peca = window.prompt("Digite novamente o nome da peça:");
 
+        }
+
+        if (peca.length < 3) {
+            alert("[ATENÇÃO] Cadastro encerrado por esgotamento de tentativas!")
+        } else
+
+            var peso = Number(window.prompt(`Digite o peso da peça "${peca}" em gramas:`));
+
+        if (peso < 100) {
+            window.confirm(`* ------- Peça deverá possuir peso superior a 100g! ------- *
+                    [ATENÇÃO] 1 tentativa restante!`)
+
+            peso = Number(window.prompt(`Digite novamente o peso da peça "${peca}" em gramas:`));
+
+        }
+        if (peso < 100) {
+            alert("[ATENÇÃO] Cadastro encerrado por esgotamento de tentativas!")
+        } else {
+
+            listaDePecas.push({
+                desc: peca,
+                peso: peso
+            });
+
+        }
     }
+
+    if (listaDePecas.length != 0)
+        alert("[Parabéns] Peças cadastradas com sucesso!")
 }
-// Mostrar peças inseridas
 
-for (let i = 0; i < tentativas; i++) {
+// Mostar peças
 
+for (var i = 0; i < quantidadeLista; i++) {
     document.write(`
     Peça: ${listaDePecas[i].desc} | Peso: ${listaDePecas[i].peso}g <br>`)
 
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Atividades do curso "Programador Front-end"
 
 ## Significado nos commits
 
--	:pencil2: Atualização
+- :pencil2: Atualização
 - :x: Remoção
 - :heavy_check_mark: Concluído
 - :open_file_folder: Organização de pastas/arquivos
