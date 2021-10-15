8768bc4ed028198ba8c30b83d8736892f50317f1
Melqui Santos <melqsantos96@gmail.com>
2021-10-15 16:34:28 -0300

    :heavy_check_mark: Simulação sistema de login

--- 2 - CODIFICAÇÃO PARA FRONT/Atividades/03-javascript/js/script.js
+++ 2 - CODIFICAÇÃO PARA FRONT/Atividades/03-javascript/js/script.js
@@ -1,4 +1,50 @@
-// Pega o ID do botão clicado e formata o nome que será igual do ID do produto. 
+/* Mostra o nome do usuário caso a storage no navegador tenha conteúdo.*/
+var body = document.querySelector("body");
+var usu = document.getElementById("login");
+
+body.onload = function() {
+    if (typeof sessionStorage.nome != "undefined") {
+        usu.innerHTML = "Bem vindo(a) " + "<strong>" + sessionStorage.nome + "</strong>";
+    }
+}
+
+
+/* Função para validar o login na página principal */
+function validaLogin() {
+    const usuarios = [
+        { nome: "Daniel", senha: "123456" },
+        { nome: "Melqui", senha: "123456" },
+        { nome: "Julia", senha: "123456" },
+        { nome: "Marcos", senha: "123456" }
+    ];
+
+    var alerta = document.getElementById("alerta");
+    var nome = document.getElementById("name").value;
+    var senha = document.getElementById("senha").value;
+    var entrar = document.getElementById("entrar");
+    var encontrado = 0;
+
+    // Percorre a lista de usuário e verifica se o usuário/senha digitados existem.
+    for (var x = 0; x < usuarios.length; x++) {
+        if (nome == usuarios[x].nome && senha == usuarios[x].senha) {
+            encontrado = encontrado + 1;
+        }
+    }
+
+    // Caso não encontra apresenta um erro para o usuário.
+    if (encontrado == 0) {
+        alerta.innerHTML = "<i class='fas fa-times-circle'></i> Usuário ou senha incorretos."
+    } else {
+        // Armazena o nome do usuário na storage do navegador.
+        sessao = sessionStorage;
+        sessao.setItem("nome", nome);
+        window.location.href = "index.html";
+
+    }
+}
+
+/* Função para contagem de itens salvos 
+Pega o ID do botão clicado e formata o nome que será igual do ID do produto. */
 function salvarItem(idBotao) {
 
     if (typeof itensSalvos === "undefined") {
