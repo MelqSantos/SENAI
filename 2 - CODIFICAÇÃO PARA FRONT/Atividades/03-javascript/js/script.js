c4af9c3582eb5d4ff190a9ec07c5fb32a205e43e
Melqui Santos <melqsantos96@gmail.com>
2021-10-20 00:56:30 -0300

    :heavy_check_mark: Finalização cesta de compras

--- 2 - CODIFICAÇÃO PARA FRONT/Atividades/03-javascript/js/script.js
+++ 2 - CODIFICAÇÃO PARA FRONT/Atividades/03-javascript/js/script.js
@@ -36,12 +36,12 @@ function login() {
     }
 }
 
+
 /* -------------------- Funções relacionadas à Itens salvos -------------------- */
 
 /* Função para contagem de itens salvos 
 Pega o ID do botão clicado e formata o nome que será igual do ID do produto. */
 function salvarItem(idBotao) {
-    debugger
 
     if (typeof itensSalvos === "undefined") {
         // Variável global.
@@ -51,8 +51,8 @@ function salvarItem(idBotao) {
     var idIitem = idBotao.replace("btn", "item");
     var itemHTML = document.getElementById(idIitem);
     var salvos = document.getElementById("salvos");
+    var cesta = document.getElementById("cesta");
     var id = document.getElementById(idBotao).value;
-
 
     // Adiciona em uma lista a estrutura HTML do item selecionado.
     itensSalvos.push({
@@ -63,6 +63,7 @@ function salvarItem(idBotao) {
     let index = itensSalvos.findIndex(i => i.id == id);
     alert(`${itensSalvos[index].dados.children[1].textContent} - Salvo com sucesso!`)
     salvos.innerText = itensSalvos.length;
+    cesta.innerText = itensSalvos.length;
 }
 
 /* Função para mostrar os itens salvos pelo usuário */
@@ -96,6 +97,69 @@ function mostrarSalvos() {
             lista.appendChild(clone)
             window.location.href = "#item"
         }
+    }
+
+}
+
+
+/* -------------------- Funções relacionadas à Cesta de compras -------------------- */
+
+// Função para montar e mostrar os itens salvos em uma tabela, ao clicar no ícone da cesta.
+function cesta() {
+
+    cestaCompras = document.getElementById("corpo-tabela");
+    var total = 0;
+
+    if (typeof itensSalvos != "undefined") {
+
+        // Limpa a tabela antes de adicionar os itens
+        document.getElementById("cesta-vazia").style.display = "none";
+        document.getElementById("corpo-tabela").innerHTML = "";
+
+        // Mostra novamente a tabela caso seja necessário
+        document.getElementById("titulo-tabela").style.display = "table-row";
+
+        for (var x = 0; x < itensSalvos.length; x++) {
+            var id = itensSalvos[x].id;
+            var nomeItem = itensSalvos[x].dados.children[1].textContent;
+            document.getElementById("corpo-tabela").display = "block";
+
+            // Formatação do valor dos itens adicionados na cesta de compras.
+            var preco = itensSalvos[x].dados.children[3].textContent;
+            var precoFormatado = preco.split(" ");
+            precoFormatado = precoFormatado[1].replaceAll(".", "");
+            var precoReal = precoFormatado.replaceAll(",", ".")
+            preco = parseFloat(precoReal);
+            var total = total + preco;
+
+            // Cria o elemento "tr" (linha), adiciona um ID e incrementa esse elemento na tabela.
+            var linha = document.createElement("tr");
+            linha.setAttribute("id", "tr" + x)
+            cestaCompras.appendChild(linha);
+
+            // Cria elemento "td", adiciona o ID no elemento e adiciona na 1° célula. 
+            var celula = document.createElement("td");
+            celula.innerHTML = id;
+            document.getElementById("tr" + x).appendChild(celula);
+
+            // Cria elemento "td", adiciona o Nome do item no elemento e adiciona na 2° célula.
+            celula = document.createElement("td");
+            celula.innerHTML = nomeItem;
+            document.getElementById("tr" + x).appendChild(celula);
+
+            // Cria elemento "td", adiciona o Preço do item no elemento e adiciona na 3° célula.
+            celula = document.createElement("td");
+            celula.innerHTML = preco.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
+            document.getElementById("tr" + x).appendChild(celula);
+
+        }
+
+        var totalPreco = document.getElementById("total");
+        totalPreco.innerHTML = `Total do pedido: <strong>${total.toLocaleString("pt-br", { style: "currency", currency: "BRL" })}</strong>`
+
+    } else {
+        // Esconde a tabela caso não tenha nenhum item adicionado a cesta.
+        document.getElementById("titulo-tabela").style.display = "none";
     }
 
 }
